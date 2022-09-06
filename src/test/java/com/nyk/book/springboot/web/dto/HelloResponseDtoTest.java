4977e9c673106379772da83cdcccbfcc29d22889
NYK <kny0339@gmail.com>
2022-09-06 16:06:34 +0900

    HelloController, HelloResponseDto 및 단위테스트

---
+++ src/test/java/com/nyk/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.nyk.book.springboot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void is_lombokOk() {
+        //given
+        String name = "test";
+        int amount = 1000;
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+        //then
+        assertThat(dto.getName().equals(name));
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+}
