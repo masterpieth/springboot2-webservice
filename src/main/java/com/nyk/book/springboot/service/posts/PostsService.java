56597b119981b3575cf3356290c37f2dfaae09f5
NYK <kny0339@gmail.com>
2022-09-09 13:06:15 +0900

    게시글 등록, 조회

--- src/main/java/com/nyk/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/nyk/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.nyk.book.springboot.service.posts;
 
 import com.nyk.book.springboot.domain.posts.Posts;
 import com.nyk.book.springboot.domain.posts.PostsRepository;
+import com.nyk.book.springboot.web.dto.PostsListResponseDto;
 import com.nyk.book.springboot.web.dto.PostsResponseDto;
 import com.nyk.book.springboot.web.dto.PostsSaveRequestDto;
 import com.nyk.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()
                 -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
